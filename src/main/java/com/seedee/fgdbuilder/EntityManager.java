8c8dcdbbeaa3c1b5b25d3d253217e986dda80df1
seedee <cdaniel9000@gmail.com>
2023-12-28 03:34:08 +0000

    Use static block for regex patterns, add pattern for J.A.C.K. flags

--- src/main/java/com/seedee/fgdbuilder/EntityManager.java
+++ src/main/java/com/seedee/fgdbuilder/EntityManager.java
@@ -15,20 +15,18 @@ import java.util.regex.Pattern;
  */
 public class EntityManager {
     
+    private static final Pattern[] entityPatterns;
+    private static final Pattern[] entityPropertyPatterns;
     private File fgdFile;
     private final EnumMap<EntityType, ArrayList<Entity>> entityListMap = new EnumMap<>(EntityType.class);
-    private final Pattern[] entityPatterns;
-    private final Pattern[] entityPropertyPatterns;
     
-    public EntityManager() {
-        for (EntityType entClass : EntityType.values())
-            entityListMap.put(entClass, new ArrayList<>());
-        
+    static {
         entityPatterns = new Pattern[] {
             Pattern.compile("(^@.*?Class)", Pattern.CASE_INSENSITIVE),
             Pattern.compile("=\\s*([^:\\n\\[\\]]+)", Pattern.CASE_INSENSITIVE),
             Pattern.compile(":\\s*\"([^\"\\[]*)", Pattern.CASE_INSENSITIVE),
             Pattern.compile("base\\(([^)]*)\\)", Pattern.CASE_INSENSITIVE),
+            Pattern.compile("flags\\(([^)]*)\\)", Pattern.CASE_INSENSITIVE),
             Pattern.compile("size\\(\\s*([-+]?\\d+)\\s+([-+]?\\d+)\\s+([-+]?\\d+)\\s*,\\s*([-+]?\\d+)\\s+([-+]?\\d+)\\s+([-+]?\\d+)\\s*\\)", Pattern.CASE_INSENSITIVE),
             Pattern.compile("color\\(\\s*([-+]?\\d+)\\s+([-+]?\\d+)\\s+([-+]?\\d+)\\s*\\)", Pattern.CASE_INSENSITIVE),
             Pattern.compile("iconsprite\\(\"*([^\"]*)\"*\\)", Pattern.CASE_INSENSITIVE),
@@ -39,6 +37,11 @@ public class EntityManager {
         entityPropertyPatterns = new Pattern[] {
             Pattern.compile("(.+)\\((.+)\\)", Pattern.CASE_INSENSITIVE)
         };
+    }
+    
+    public EntityManager() {
+        for (EntityType entClass : EntityType.values())
+            entityListMap.put(entClass, new ArrayList<>());
     }
     
     public void setFgdFile(File fgdFile) {
